a71ad9656f8c26b72696293b5d4a805aa39fffe3
Almog <ohayonalmog7@gmail.com>
2023-03-01 10:54:50 +0200

    first commit

---
+++ cmps/AppHeader.js
@@ -0,0 +1,27 @@
+export default {
+	template: `
+        <header class="app-header">
+            <h1>AppSus</h1>
+            <nav>
+                <RouterLink v-for="(route, idx) in routes" :to="route.path" 
+                :title="route.title" :key="idx">{{route.title}} </RouterLink> 
+            </nav>
+        </header>
+    `,
+    data(){
+        return{
+            routes: [
+                 {path:'/', title:'Home'},
+                 {path:'/notes', title:'Notes'},
+                 {path:'/mail', title:'Mail'},
+             ]
+     
+         }
+    },
+    methods: {
+        setRoute(route) {
+            this.$emit('set-route', route)
+        }
+    }
+
+}
---
+++ routes.js
@@ -0,0 +1,30 @@
+import HomePage from './views/HomePage.js'
+import AboutUs from './views/AboutUs.js'
+import NoteIndex from './apps/keep/pages/NoteIndex.js'
+import MailIndex from './apps/mail/pages/MailIndex.js'
+
+const { createRouter, createWebHashHistory } = VueRouter
+
+const routerOptions = {
+	history: createWebHashHistory(),
+	routes: [
+		{
+			path: '/',
+			component: HomePage,
+		},
+		{
+			path: '/about',
+			component: AboutUs,
+		},
+		{
+			path: '/notes',
+			component: NoteIndex,
+		},
+		{
+			path: '/mail',
+			component: MailIndex,
+		},
+	],
+}
+
+export const router = createRouter(routerOptions)

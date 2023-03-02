0a96d5ff7b032183a08930bbbe9423854ef3be37
Almog <ohayonalmog7@gmail.com>
2023-03-02 14:43:07 +0200

    adding svg

--- apps/mail/cmps/EmailPreview.js
+++ apps/mail/cmps/EmailPreview.js
@@ -1,22 +1,25 @@
 import { eventBus } from './../../../services/event-bus.service.js'
 import { emailService } from './../services/email.service.js'
+import { svgService } from '../services/svg.service.js'
 
 export default {
     name: 'emailPreview',
     props: ['email'],
     template: `
-        <div class="email-preview-container" @click="setDetails(this.email)" :class="{isRead:email.isRead}">
+        <div class="email-preview-container" @click="setDetails(this.email)" :class="{isRead:!email.isRead}">
             <input type="checkbox" class="email-preview-checkbox"/>
-            <div class="check-star"><!-- TODO: star svg here --></div>
+            <i class="check-star" v-html="getSvg('star')"></i>
             <p class="email-preview from">{{email.from}}</p>
             <div class="prev">
                 <p class="email-preview subject">{{email.subject}}</p>
                 <p class="email-preview body">{{email.body}}</p>
             </div>
             <p class="email-preview time">{{email.sentAt}}</p>
-            <p class="email-preview icons"></p>
-              <!-- TODO: svg here :hover -->
-           </div>
+            <div class="email-preview icons">
+                <i class="email-preview-icon archive" v-html="getSvg('archive')"></i>
+                <i class="email-preview-icon delete" v-html="getSvg('delete')"></i>
+                <i class="email-preview-icon markasread" v-html="getSvg('mark_as_read')"></i>
+            </div>
         </div>
     `,
     methods: {
@@ -35,5 +38,8 @@ export default {
                 this.$emit('remove', email.id)
             }
         },  
+        getSvg(iconName) {
+            return svgService.getSvg(iconName)
+        },
     },
 }

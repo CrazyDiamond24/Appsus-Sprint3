623732623d2c70b8ae9d80cf463fdadb46606bdf
rania <raniakittan@hotmail.com>
2023-03-04 21:25:42 +0200

    fixed a small issue

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -1,26 +1,43 @@
 export default {
-	template: `
-<section class="about-page">
-  <div class="sunflower sunflower-topright"></div>
-  <div class="sunflower sunflower-bottomleft"></div>
-  <div class="about-container">
-    <div class="about-image">
-      <img src="./assets/img/almogg.jpg" alt="Almog Gavra">
-    </div>
-    <div class="about-text">
-      <h2>Almog Ohayon</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel euismod turpis. Vivamus interdum consectetur commodo. Aliquam tristique neque quis lectus semper ultricies. Curabitur scelerisque velit id augue bibendum, sed dictum lorem vestibulum. Fusce gravida, risus in luctus rhoncus, orci dolor eleifend mauris, et auctor metus augue sit amet mauris.</p>
-    </div>
-  </div>
-  <div class="about-container">
-    <div class="about-image">
-      <img src="./assets/img/rania.jpg" alt="Rania Abu Safiya">
-    </div>
-    <div class="about-text">
-      <h2>Rania Elizabeth Kittan</h2>
-      <p>Hi, I'm Rania! A lover of all things creative - from drawing and art to literature and code. As a former English teacher, I've always had a passion for language and expression, and my journey has led me to explore different mediums of self-expression. You can usually find me listening to Pink Floyd, spending time with my two cats, or just being my slightly weird but always genuine self.</p>
-    </div>
-  </div>
-</section>
-    `
+  template: `
+    <section class="about-page"
+      @click="activateAboutPage"
+      @mouseleave="deactivateAboutPage"
+    >
+      <div class="sunflower sunflower-topright"></div>
+      <div class="sunflower sunflower-bottomleft"></div>
+      <div class="about-container">
+        <div class="about-image">
+          <img src="./assets/img/almogg.jpg" alt="Almog Gavra">
+        </div>
+        <div class="about-text">
+          <h2>Almog Ohayon</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel euismod turpis. Vivamus interdum consectetur commodo. Aliquam tristique neque quis lectus semper ultricies. Curabitur scelerisque velit id augue bibendum, sed dictum lorem vestibulum. Fusce gravida, risus in luctus rhoncus, orci dolor eleifend mauris, et auctor metus augue sit amet mauris.</p>
+        </div>
+      </div>
+      <div class="about-container">
+        <div class="about-image">
+          <img src="./assets/img/rania.jpg" alt="Rania Abu Safiya">
+        </div>
+        <div class="about-text">
+          <h2>Rania Elizabeth Kittan</h2>
+          <p>Hi, I'm Rania! A lover of all things creative - from drawing and art to literature and code. As a former English teacher, I've always had a passion for language and expression, and my journey has led me to explore different mediums of self-expression. You can usually find me listening to Pink Floyd, spending time with my two cats, or just being my slightly weird but always genuine self.</p>
+        </div>
+      </div>
+    </section>
+    `,
+  methods: {
+    activateAboutPage() {
+      document.body.classList.add('about-page-active')
+    },
+    deactivateAboutPage() {
+      document.body.classList.remove('about-page-active')
+    },
+  },
+  mounted() {
+    this.activateAboutPage()
+  },
+  destroyed() {
+    this.deactivateAboutPage()
+  },
 }

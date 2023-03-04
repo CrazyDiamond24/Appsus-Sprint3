2db17c223936d1260fbe95e1c32c9af35469ed86
rania <raniakittan@hotmail.com>
2023-03-04 20:24:59 +0200

    added about me

--- views/AboutUs.js
+++ views/AboutUs.js
@@ -18,7 +18,7 @@ export default {
     </div>
     <div class="about-text">
       <h2>Rania Elizabeth Kittan</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel euismod turpis. Vivamus interdum consectetur commodo. Aliquam tristique neque quis lectus semper ultricies. Curabitur scelerisque velit id augue bibendum, sed dictum lorem vestibulum. Fusce gravida, risus in luctus rhoncus, orci dolor eleifend mauris, et auctor metus augue sit amet mauris.</p>
+      <p>Hi, I'm Rania! A lover of all things creative - from drawing and art to literature and code. As a former English teacher, I've always had a passion for language and expression, and my journey has led me to explore different mediums of self-expression. You can usually find me listening to Pink Floyd, spending time with my two cats, or just being my slightly weird but always genuine self.</p>
     </div>
   </div>
 </section>

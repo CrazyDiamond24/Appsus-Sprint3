769b62b8ef54b94d97fa8e9a09583d5872883ee1
rania <raniakittan@hotmail.com>
2023-03-04 15:08:33 +0200

    fixed issue with header

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -3,14 +3,14 @@ export default {
     <header class="app-header">
       <RouterLink to="/">
         <div class="logo-container">
-        <img class="logo-img" src="./assets/img/A.png" alt="A">
-        <img class="logo-img" src="./assets/img/P.png" alt="P">
-        <img class="logo-img" src="./assets/img/P.png" style="margin-right: -5px;" alt="P">
-        <img class="logo-img" src="./assets/img/S.png" alt="P">
-        <img class="logo-img" src="./assets/img/U.png" style="margin-right: -5px;" alt="S">
-        <img class="logo-img" src="./assets/img/S.png" alt="U">
-</RouterLink>
+        <img class="logo-img" src="./assets/img/A.png" alt="A"/>
+        <img class="logo-img" src="./assets/img/P.png" alt="P"/>
+        <img class="logo-img" src="./assets/img/P.png" style="margin-right: -5px;" alt="P"/>
+        <img class="logo-img" src="./assets/img/S.png" alt="P"/>
+        <img class="logo-img" src="./assets/img/U.png" style="margin-right: -5px;" alt="S"/>
+        <img class="logo-img" src="./assets/img/S.png" alt="U"/>
       </div>
+    </RouterLink>
       <nav>
         <RouterLink v-for="(route, idx) in routes" :to="route.path" :title="route.title" :key="idx" exact-active-class="active-link">{{route.title}} </RouterLink>
       </nav>

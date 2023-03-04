ef0df03ec797e9dec4241e22ad50c0c92f409206
rania <raniakittan@hotmail.com>
2023-03-04 15:05:16 +0200

    fixed img pathing in header

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -4,11 +4,11 @@ export default {
       <RouterLink to="/">
         <div class="logo-container">
         <img class="logo-img" src="./assets/img/A.png" alt="A">
-        <img class="logo-img" src="./assets/img/P.PNG" alt="P">
-        <img class="logo-img" src="./assets/img/P.PNG" style="margin-right: -5px;" alt="P">
-        <img class="logo-img" src="./assets/img/S.PNG" alt="P">
-        <img class="logo-img" src="./assets/img/U.PNG" style="margin-right: -5px;" alt="S">
-        <img class="logo-img" src="./assets/img/S.PNG" alt="U">
+        <img class="logo-img" src="./assets/img/P.png" alt="P">
+        <img class="logo-img" src="./assets/img/P.png" style="margin-right: -5px;" alt="P">
+        <img class="logo-img" src="./assets/img/S.png" alt="P">
+        <img class="logo-img" src="./assets/img/U.png" style="margin-right: -5px;" alt="S">
+        <img class="logo-img" src="./assets/img/S.png" alt="U">
 </RouterLink>
       </div>
       <nav>

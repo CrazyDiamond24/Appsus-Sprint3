a2d4313204a1064a322341de2af92fc61a7f064e
rania <raniakittan@hotmail.com>
2023-03-04 14:29:28 +0200

    changed pathing

--- cmps/AppHeader.js
+++ cmps/AppHeader.js
@@ -2,12 +2,12 @@ export default {
 	template: `
     <header class="app-header">
       <div class="logo-container">
-        <img class="logo-img" src="../../../assets/img/A.png" alt="A">
-        <img class="logo-img" src="../../../assets/img/P.PNG" alt="P">
-        <img class="logo-img" src="../../../assets/img/P.PNG" style="margin-right: -5px;" alt="P">
-        <img class="logo-img" src="../../../assets/img/S.PNG" alt="P">
-        <img class="logo-img" src="../../../assets/img/U.PNG" style="margin-right: -5px;" alt="S">
-        <img class="logo-img" src="../../../assets/img/S.PNG" alt="U">
+        <img class="logo-img" src="./assets/img/A.png" alt="A">
+        <img class="logo-img" src="./assets/img/P.PNG" alt="P">
+        <img class="logo-img" src="./assets/img/P.PNG" style="margin-right: -5px;" alt="P">
+        <img class="logo-img" src="./assets/img/S.PNG" alt="P">
+        <img class="logo-img" src="./assets/img/U.PNG" style="margin-right: -5px;" alt="S">
+        <img class="logo-img" src="./assets/img/S.PNG" alt="U">
       </div>
       <nav>
         <RouterLink v-for="(route, idx) in routes" :to="route.path" :title="route.title" :key="idx" exact-active-class="active-link">{{route.title}} </RouterLink>
--- views/HomePage.js
+++ views/HomePage.js
@@ -4,15 +4,15 @@ export default {
   <div class="content">
     <div class="image-grid">
       <router-link class="image-item" to="/email/list">
-        <img src="../../../assets/img/sus-mail.jpg" alt="Email">
+        <img src="./assets/img/sus-mail.jpg" alt="Email">
         <h2>Mail ✉️</h2>
       </router-link>
       <router-link class="image-item" to="/notes">
-        <img src="../../../assets/img/sus-note.jpg" alt="Notes">
+        <img src="./assets/img/sus-note.jpg" alt="Notes">
         <h2>Notes 📝</h2>
       </router-link>
       <router-link class="image-item" to="/books">
-        <img src="../../../assets/img/sus-book.jpg" alt="Books">
+        <img src="./assets/img/sus-book.jpg" alt="Books">
         <h2>Books 📚</h2>
       </router-link>
     </div>
